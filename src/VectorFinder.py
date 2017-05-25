6a3292624826fd898a2355fab7686ab8109ae142
Vindula Jayawardana <vindulamj@gmail.com>
2017-05-25 22:24:24 +0530

    Update VectorFinder.py

--- src/VectorFinder.py
+++ src/VectorFinder.py
@@ -1,28 +1,14 @@
-import re
-
-f = open('../data/text8-vector.txt', "r")
-searchlines = f.readlines()
-f.close()
+from gensim.models.keyedvectors import KeyedVectors
 
 listoflists = []
-
-def ConvertStringToFloat(str):
-   for k in range(1,len(str),1):
-	str[k] = float(str[k])
-   return str
-
+model = KeyedVectors.load_word2vec_format('../data/text8-vector.bin', binary=True)
 
 def VectorFinder(word_list):
 	wordList = word_list
 	for word in wordList:
-		for i, line in enumerate(searchlines):
-    			if (re.findall('\\b'+word+'\\b', line)): 
-        			for l in searchlines[i:i+1]: 
-        				splitted_list =l.split()
-					splitted_list = ConvertStringToFloat(splitted_list)
-		listoflists.append(splitted_list)
+		v =  model[word]  # raw numpy vector of a word
+		listoflists.append(v)
 	return listoflists;
-
 
 
 
