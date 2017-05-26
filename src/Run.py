d79a62dc710a874218b5da5ee3cb09477feaf0ff
Vindula Jayawardana <vindulamj@gmail.com>
2017-05-26 09:08:01 +0530

    Update Run.py

--- src/Run.py
+++ src/Run.py
@@ -1,5 +1,10 @@
 from VectorFinder import *
 
-wordlist = ["gone", "the"]
-returnList = VectorFinder(wordlist)
+with open("word.txt", "r") as ins:
+    array = []
+    for line in ins:
+        array.append(line.rstrip())
+del array[0]
+
+returnList = VectorFinder(array)
 print returnList
