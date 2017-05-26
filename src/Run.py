7559293f65508a989e22c9c08ed9ae343db2e91e
Vindula Jayawardana <vindulamj@gmail.com>
2017-05-26 09:53:05 +0530

    Update Run.py

--- src/Run.py
+++ src/Run.py
@@ -1,10 +1,12 @@
 from VectorFinder import *
 
-with open("word.txt", "r") as ins:
+file = open("../data/vectors.txt","w") 
+with open("../data/word.txt", "r") as ins:
     array = []
     for line in ins:
         array.append(line.rstrip())
 del array[0]
 
 returnList = VectorFinder(array)
-print returnList
+file.write(str(returnList))  
+file.close() 
