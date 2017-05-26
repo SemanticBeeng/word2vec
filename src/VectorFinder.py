55b551a64455f3c2c8d4e9d1280eb503d9751a3d
Vindula Jayawardana <vindulamj@gmail.com>
2017-05-26 09:52:46 +0530

    Update VectorFinder.py

--- src/VectorFinder.py
+++ src/VectorFinder.py
@@ -1,14 +1,28 @@
 from gensim.models.keyedvectors import KeyedVectors
 
+file = open("../data/log.txt","w") 
 listoflists = []
+listofwords = []
 model = KeyedVectors.load_word2vec_format('../data/text8-vector.bin', binary=True)
 
 def VectorFinder(word_list):
 	wordList = word_list
 	for word in wordList:
-		v =  model[word]  # raw numpy vector of a word
-		listoflists.append(v)
+		try:
+			v =  model[word]  # raw numpy vector of a word
+			listofwords.append(word)
+			listoflists.append(v)
+			file.write("Word "+ word + " is sucessfully found in the model.\n")  
+		except:
+			print "Word " + word + " is not in the trained model."
+			file.write("Word " + word + " is not in the trained model\n")  
+	file.close()
+	listoflists.append(listofwords)
 	return listoflists;
+
+ 
+
+
 
 
 
